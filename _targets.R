8935af86c337fc9a4d1e046bda16df07dce049fe
Kaitlyn Johnson <kej38@georgetown.edu>
2025-04-04 15:24:07 +0100

    add code back in to outlined targets file

--- _targets.R
+++ _targets.R
@@ -35,46 +35,42 @@ tar_option_set(
   # have whole pipeline fail
 )
 
-# Methods
+# Methods---------------------------------------------------------------------
 
 # We'll start by gathering all of the datasets and model specifications that we
 # need for each of the component analyses
 
-## Real-world data from German Nowcast Hub
+## Real-world data from German Nowcast Hub------------------------------------
+
+### Create long tidy dataframe as of final date from German Nowcast Hub------
+# This will be used to generate reporting triangles as of nowcast dates 
+# and to generate evaluation data (triangle as of nowcast date + eval time)
 data_targets <- list(
 
-  ### Create reporting triangles as of daily nowcast dates
 
-  ### Create evaluation data
-  # This will be as of each of the nowcast dates + the evaluation time frame (e.g.
-  # 80 days)
+### Model specification for German nowcast data------------------------------
+# Specify the maximum delay, number of reference times to use for delay
+# estimation, number of reporting triangles to use for dispersion estimate, 
+# and whether or not delay estimates/dispersion parameters are being borrowed
 
-  ### Model specification for German nowcast data
-  # Specify the maximum delay, number of reference times to use for delay
-  # estimation, number of reporting triangles to use for dispersion estimate, and
-  # whether or not delay estimates/dispersion parameters are being borrowed
+## Simulated data, modified from German Nowcast Hub----------------------------
 
-  ## Simulated data, modified from German Nowcast Hub
+### Create simulated long tidy dataframes from data above--------------------
+# This will be for two age strata only, and with a long and short
+# delay, creating 4 distinct datasets. Will be used to generate reporting
+# triangles and evaluation data
 
-  ## Create simulated reporting triangles as of nowcast dates from final reports
-  # This will be for two age strata only, and with a long and short
-  # delay, creating 4 distinct datasets
+### Model specification(s) for simulated data--------------------------------
+# Make all combinations of varying:
+# - max delay
+# - borrowing for delay estimation
+# - borrowing for dispersion estimate
+# - number of observations for delay estimation
+# - number of reporting triangles for dispersion estimate
 
-  ### Create evaluation data for simulated data
-  # This will be from simulated data as of nowcast date + eval time frame (e.g. 80
-  # days)
+## Real-world case data: Measles and norovirus-------------------------------
 
-  ### Model specification(s) for simulated data
-  # Make all combinations of varying:
-  # - max delay
-  # - borrowing for delay estimation
-  # - borrowing for dispersion estimate
-  # - number of observations for delay estimation
-  # - number of reporting triangles for dispersion estimate
-
-  ## Real-world case data: Measles and norovirus
-
-  ### Load and clean measles data
+### Load and clean measles data----------------------------------------------
   tar_target(
     name = measles_line_list,
     command = read.delim("https://raw.githubusercontent.com/kassteele/Nowcasting/refs/heads/master/data/measles_NL_2013_2014.dat", sep = "") |> # nolint
@@ -84,6 +80,10 @@ data_targets <- list(
       ) |>
       select(reference_date, report_date)
   ),
+  
+### Create long tidy dataframe up until final date---------------------------
+# Will be used to create reporting triangles as of nowcast dates + 
+# evaluation data 
   tar_target(
     name = measles_long,
     command = measles_line_list |>
@@ -105,57 +105,14 @@ data_targets <- list(
       mutate(confirm = ifelse(is.na(confirm), 0, confirm))
   ),
 
+### Load and clean norovirus data--------------------------------------------
 
-  ### Create reporting triangles as of final nowcast date
-  tar_target(
-    name = measles_nowcast_dates,
-    command = tibble(nowcast_date = c(
-      "2013-07-01", "2013-10-01",
-      "2014-02-25"
-    )) |>
-      group_by(nowcast_date) |>
-      tar_group(),
-    iteration = "group"
-  ),
-  # This function will be used to format the data as a reporting triangle
-  # from a long dataframe by report and reference date and as of a particular
-  # nowcast date
-  tar_target(
-    name = measles_rt,
-    command = do.call(
-      get_rep_tri_from_long_df,
-      c(list(
-        long_df = measles_long,
-        nowcast_date = max(measles_long$reference_date),
-        max_delay = 50
-      ))
-    )
-  ),
+### Create long tidy datframe up until final date----------------------------
+# Will be used to create reporting triangles as of nowcast dates + 
+# evaluation data 
 
-  ### Load in norovirus data
-
-  ### Create reporting triangles as of final nowcast date
-
-  ### Create summarised counts as of all nowcast dates + eval time frames
-  tar_target(
-    name = measles_nowcasts_df,
-    command = do.call(
-      get_eval_data_from_long_df,
-      c(list(
-        long_df = measles_long,
-        # replace with nowcast date + eval time frame
-        as_of_date = ymd(measles_nowcast_dates$nowcast_date) + days(50)
-      ))
-    ),
-    pattern = map(measles_nowcast_dates)
-  ),
-  tar_target(
-    name = plot_measles_data,
-    command = get_plot_data_as_of(measles_nowcasts_df),
-    format = "rds"
-  ),
-  ### Model specification for each data set
-  # Based off of the specific dataset, make a single choice about specification
+### Model specification for each data set------------------------------------
+# Based off of the specific dataset, make a single choice about specification
   tar_target(
     name = measles_spec,
     command = list(
@@ -166,49 +123,69 @@ data_targets <- list(
       borrow_dispersion = FALSE
     ),
     format = "rds"
-  ),
-  # Results
+  )
+) # end data_targets
 
-  ## Run `baselinenowcast` model
+# Results-------------------------------------------------------------------
 
-  ### Use tar_map or tar_group_by to run the model on all datasets
-  tar_map(
+## Run real-world German Nowcast Hub case study-----------------------------
+### Loop over each nowcast date and strata ----------------------------------
+# 1. Generate nowcasts and aggregate (baselinenowcast pipeline)
+# 2. Save quantiled nowcasts for visualisation 
+
+## Run simulated long delays and spare data----------------------------------
+### Loop over each nowcast date, strata, data scenario, and model spec-------
+# 1. Generate nowcasts and aggregate (baselinenowcast pipeline)
+# 2. Generate evaluation data for that nowcast date 
+# 3. Score nowcasts - X time in days and save with metadata
+# 4. Save quantiled nowcasts for visualisation 
+
+## Run measles case study----------------------------------------------------
+model_run_targets <- list(
+### Loop over each nowcast date ---------------------------------------------
+ tar_map(
     values = list(
       nowcast_date = c(
         "2013-07-01", "2013-10-01",
         "2014-02-25"
       )
     ),
-    # Get the reporting triangle as of the nowcast date
-    tar_target(
-      name = rep_tri_df,
-      command = get_rep_tri_from_long_df(
-        long_df = measles_long,
-        nowcast_date = nowcast_date,
-        max_delay = 50
-      )
+# 1. Generate nowcasts and aggregate (baselinenowcast pipeline)
+# Get the reporting triangle as of the nowcast date
+  tar_target(
+    name = rep_tri_df,
+    command = get_rep_tri_from_long_df(
+      long_df = measles_long,
+      nowcast_date = nowcast_date,
+      max_delay = 50
     )
-  ) # end tar map
-) # end model run targets
+  )
+) # end model_run targets
+# 2. Save quantiled nowcasts for visualisation 
 
+## Run norovirus case study and score----------------------------------------
+### Loop over each nowcast date ---------------------------------------------
+# 1. Generate nowcasts and aggregate (baselinenowcast pipeline)
+# 2. Generate evaluation data for that nowcast date 
+# 3. Score for nowcast data - X time in days and save with metadata
+# here use WIS for consistency with norovirus scores
+# 4. Save quantiled nowcasts for visualisation 
 
+## Gather nowcast scores for other norovirus models ------------------------
+# 1. Combine norovirus model scores by nowcast date and model type
 
+)
 #### Generate outputs for each model run joined to corresponding metadata
-# - scores (from samples)
-# - scores (from quantiles; e.g. calibration but also WIS scores)
-# - quantiled nowcasts for visual comparison
 
 ### Figures for real-world case study German Nowcast Hub
 
 ### Figures for simulated data case study with different model specifications
 
-### Figures for norovirus and measles
+### Figures for and measles
 
-### Consolidate evaluation data from mean/median ensemble in GNH & our run
-
-### Make figures comparing performance of baselinenowcats and ensembles
+### Make figures comparing performance of baselinenowcats and norovirus models
 
 list(
-  data_targets
-  # model_run_targets
+  data_targets,
+  model_run_targets
 )

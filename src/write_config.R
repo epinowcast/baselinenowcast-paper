6ce5857fa0555d8d72b03b1692041e8777db8406
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-04-15 16:30:02 +0100

    change covid eval time frame to 40 for alignment

--- src/write_config.R
+++ src/write_config.R
@@ -101,7 +101,7 @@ write_config <- function(noro_nowcast_dates = NULL,
       max_delay = 40,
       days_to_eval = 29, # 0 to - 28 horizon)
       quantiles = c(0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975),
-      eval_timeframe = 80
+      eval_timeframe = 40
     ),
     n_draws = 100,
     plot_quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975)

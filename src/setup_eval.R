a33aeee2d157cc225104d7e42b055c659270e2d2
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-06-09 14:20:01 +0100

    set up all dates and run

--- src/setup_eval.R
+++ src/setup_eval.R
@@ -1,7 +1,7 @@
 source(file.path("src", "write_config.R"))
 
 write_config(
-  noro_nowcast_dates = NULL,
+  noro_nowcast_dates = "2023-10-08",
   covid_nowcast_dates = NULL,
   age_groups_covid = NULL,
   permutations_covid = FALSE,

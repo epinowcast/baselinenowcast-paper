d0a1673a444702ee758936d80a95451958f9ec96
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-06-17 16:11:39 +0100

    outline other targets needed

--- targets/noro_comparison_targets.R
+++ targets/noro_comparison_targets.R
@@ -1,1 +1,8 @@
-noro_comparison_targets <- list()
+noro_comparison_targets <- list(
+  # Load in scores and coverage from Mellor et al
+
+  # Combines scores and coverage from other models with baselinenowcast
+  # permutations
+
+  #
+)

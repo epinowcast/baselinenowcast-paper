51d3a4ae1ab892abe2eeb817e08eaee2abf8b497
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-08-05 16:55:40 +0100

    update to run on all dates

--- src/setup_eval.R
+++ src/setup_eval.R
@@ -3,11 +3,8 @@ library(lubridate)
 source(file.path("src", "write_config.R"))
 
 write_config(
-  noro_nowcast_dates = c("2023-11-05", "2023-11-12"),
-  covid_nowcast_dates = c(
-    "2021-11-22", "2021-12-29", "2022-01-30",
-    "2022-02-27"
-  ),
+  noro_nowcast_dates = NULL,
+  covid_nowcast_dates = NULL,
   age_groups_covid = NULL,
   permutations_covid = TRUE,
   filter_ref_dates_noro = NULL
